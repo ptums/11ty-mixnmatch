645c21278c42bc1686117bd7da3eca41668be772
Bryan Robinson <bryan@sanity.io>
2021-02-06 14:44:29 -0500

    Removes build

--- build/web/.eleventy.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = function(config) {
-    config.addPassthroughCopy("style.css");
-
-    config.addCollection('posts', collection => {
-        return collection.getFilteredByTag('post').reverse();
-    });
-}
--- build/web/utils/sanityClient.js
+++
@@ -1,6 +0,0 @@
-const sanityClient = require('@sanity/client')
-module.exports = sanityClient({
-    projectId: "biqe3ly2",
-    dataset: "production",
-    useCdn: true // `false` if you want to ensure fresh data
-})
